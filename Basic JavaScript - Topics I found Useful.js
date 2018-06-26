1818b68abbceccdd69a04a092d4030778ffedea3
unknown <hermano.mark.mh@gmail.com>
2018-06-26 19:40:11 +0800

    basic js

--- Basic JavaScript - Topics I found Useful.js
+++ Basic JavaScript - Topics I found Useful.js
@@ -379,3 +379,71 @@ var ourPets = [
 ];
 ourPets[0].names[1]; // "Fluffy"
 ourPets[1].names[0]; // "Spot"
+
+// -----------------------------------------------
+// Basic JavaScript: Iterate with JavaScript While Loops
+// You can run the same code multiple times by using a loop.
+
+// The first type of loop we will learn is called a "while" loop because it runs "while" a specified condition is true and stops once that condition is no longer true.
+
+var ourArray = [];
+var i = 0;
+while(i < 5) {
+  ourArray.push(i);
+  i++;
+}
+
+// => 4
+
+// -----------------------------------------------
+// Basic JavaScript: Iterate Through an Array with a For Loop
+// A common task in JavaScript is to iterate through the contents of an array. One way to do that is with a for loop. This code will output each element of the array arr to the console:
+
+var arr = [10,9,8,7,6];
+for (var i = 0; i < arr.length; i++) {
+   console.log(arr[i]);
+}
+// Remember that Arrays have zero-based numbering, which means the last index of the array is length - 1. Our condition for this loop is i < arr.length, which stops when i is at length - 1.
+
+// Another sample
+// Declare and initialize a variable total to 0. Use a for loop to add the value of each element of the myArr array to total.
+var myArr = [ 2, 3, 4, 5, 6];
+
+// Only change code below this line
+var total = 0;
+for (var i = 0; i < myArr.length; i++) {
+  total += myArr[i]
+}
+
+// -----------------------------------------------
+// Basic JavaScript: Nesting For Loops
+// If you have a multi-dimensional array, you can use the same logic as the prior waypoint to loop through both the array and any sub-arrays. Here is an example:
+
+var arr = [
+  [1,2], [3,4], [5,6]
+];
+for (var i=0; i < arr.length; i++) {
+  for (var j=0; j < arr[i].length; j++) {
+    console.log(arr[i][j]);
+  }
+}
+// This outputs each sub-element in arr one at a time. Note that for the inner loop, we are checking the .length of arr[i], since arr[i] is itself an array.
+
+// Q. Modify function multiplyAll so that it multiplies the product variable by each number in the sub-arrays of arr
+
+// A.
+function multiplyAll(arr) {
+  debugger;
+  var product = 1;
+  // Only change code below this line
+  for (var i = 0; i < arr.length; i++ ) {
+    for (var j = 0; j < arr[i].length; j++ ) {
+      product *= arr[i][j];
+    }
+  }
+  // Only change code above this line
+  return product;
+}
+
+// Modify values below to test your code
+multiplyAll([[1,2],[3,4],[5,6,7]]);
