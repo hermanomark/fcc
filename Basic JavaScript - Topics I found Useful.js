f0a6fa466e8bfe096c01e8de20a2e951928a2778
unknown <hermano.mark.mh@gmail.com>
2018-06-27 11:34:44 +0800

    basic js

--- Basic JavaScript - Topics I found Useful.js
+++ Basic JavaScript - Topics I found Useful.js
@@ -510,3 +510,161 @@ do {
 
 myArray;
 // => [10];
+
+// -----------------------------------------------
+// Basic JavaScript: Generate Random Fractions with JavaScript
+// Random numbers are useful for creating random behavior.
+
+// JavaScript has a Math.random() function that generates a random decimal number between 0 (inclusive) and not quite up to 1 (exclusive). Thus Math.random() can return a 0 but never quite return a 1
+
+// Change randomFraction to return a random number instead of returning 0.
+
+function randomFraction() {
+
+  // Only change code below this line.
+
+  return Math.random(0);
+
+  // Only change code above this line.
+}
+
+randomFraction();
+// => 0.8808257977087932
+
+// -----------------------------------------------
+// Basic JavaScript: Generate Random Whole Numbers with JavaScript
+// It's great that we can generate random decimal numbers, but it's even more useful if we use it to generate random whole numbers.
+
+// Use Math.random() to generate a random decimal.
+// Multiply that random decimal by 20.
+// Use another function, Math.floor() to round the number down to its nearest whole number.
+// Remember that Math.random() can never quite return a 1 and, because we're rounding down, it's impossible to actually get 20. This technique will give us a whole number between 0 and 19.
+
+// Putting everything together, this is what our code looks like:
+
+Math.floor(Math.random() * 20);
+
+// We are calling Math.random(), multiplying the result by 20, then passing the value to Math.floor() function to round the value down to the nearest whole number.
+
+// Use this technique to generate and return a random whole number between 0 and 9.
+
+function randomWholeNum() {
+
+  // Only change code below this line.
+
+  return Math.floor(Math.random() * 10);
+}
+
+randomWholeNum();
+// => 7
+
+// -----------------------------------------------
+// Basic JavaScript: Generate Random Whole Numbers within a Range
+// Instead of generating a random number between zero and a given number like we did before, we can generate a random number that falls within a range of two specific numbers.
+
+// To do this, we'll define a minimum number min and a maximum number max.
+
+// Here's the formula we'll use. Take a moment to read it and try to understand what this code is doing:
+
+Math.floor(Math.random() * (max - min + 1)) + min
+
+// Q. Create a function called randomRange that takes a range myMin and myMax and returns a random number that's greater than or equal to myMin, and is less than or equal to myMax, inclusive.
+
+// A. 
+
+function randomRange(myMin, myMax) {
+  debugger;
+  return Math.floor(Math.random() * (myMax - myMin + 1)) + myMin; // Change this line
+
+}
+
+// Change these values to test your function
+var myRandom = randomRange(5, 15);
+// => 8
+
+// -----------------------------------------------
+// Basic JavaScript: Use the parseInt Function with a Radix
+// The parseInt() function parses a string and returns an integer. It takes a second argument for the radix, which specifies the base of the number in the string. The radix can be an integer between 2 and 36.
+
+// The function call looks like:
+
+parseInt(string, radix);
+
+// And here's an example:
+
+var a = parseInt("11", 2);
+
+// The radix variable says that "11" is in the binary system, or base 2. This example converts the string "11" to an integer 3.
+
+// Q. Use parseInt() in the convertToInteger function so it converts a binary number to an integer and returns it.
+function convertToInteger(str) {
+        return parseInt(str, 2);
+
+}
+
+convertToInteger("10011");
+// => 19
+
+// -----------------------------------------------
+// Basic JavaScript: Use the Conditional (Ternary) Operator
+// The conditional operator, also called the ternary operator, can be used as a one line if-else expression.
+
+// The syntax is:
+
+// condition ? statement-if-true : statement-if-false;
+
+// The following function uses an if-else statement to check a condition:
+
+function findGreater(a, b) {
+  if(a > b) {
+    return "a is greater";
+  }
+  else {
+    return "b is greater";
+  }
+}
+// This can be re-written using the conditional operator:
+
+function findGreater(a, b) {
+  return a > b ? "a is greater" : "b is greater";
+}
+
+// Use the conditional operator in the checkEqual function to check if two numbers are equal or not. The function should return either true or false.
+
+function checkEqual(a, b) {
+  return a === b ? true : false;
+}
+
+checkEqual(1, 2);
+
+// -----------------------------------------------
+// Basic JavaScript: Use Multiple Conditional (Ternary) Operators
+// In the previous challenge, you used a single conditional operator. You can also chain them together to check for multiple conditions.
+
+// The following function uses if, else if, and else statements to check multiple conditions:
+
+function findGreaterOrEqual(a, b) {
+  if(a === b) {
+    return "a and b are equal";
+  }
+  else if(a > b) {
+    return "a is greater";
+  }
+  else {
+    return "b is greater";
+  }
+}
+// The above function can be re-written using multiple conditional operators:
+
+function findGreaterOrEqual(a, b) {
+  return (a === b) ? "a and b are equal" : (a > b) ? "a is greater" : "b is greater";
+}
+
+// Aother sample
+function checkSign(num) {
+  return (num > 0) ? "positive" : (num < 0) ? "negative" : "zero";
+}
+
+checkSign(10);
+
+// -----------------------------------------------
