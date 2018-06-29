a48ec0543177dd12493422a7cc2771f589befa6c
unknown <hermano.mark.mh@gmail.com>
2018-06-29 18:22:47 +0800

    basic js algorithm

--- Basic Algorithm Scripting - Challenges.js
+++ Basic Algorithm Scripting - Challenges.js
@@ -253,3 +253,132 @@ booWho(null);
 // booWho("a") should return false.
 // booWho("true") should return false.
 // booWho("false") should return false.
+
+// -----------------------------------------------
+// Basic Algorithm Scripting: Title Case a Sentence
+// Return the provided string with the first letter of each word capitalized. Make sure the rest of the word is in lower case.
+
+// For the purpose of this exercise, you should also capitalize connecting words like "the" and "of".
+
+function titleCase(str) {
+  let arr = str.split(" ");
+  let newArr = [];
+  for (let i=0; i < arr.length; i++ ) {
+    newArr.push(arr[i].toLowerCase());
+  }
+  let secArr = [];
+  for (let i=0; i < newArr.length; i++) {
+    secArr.push(newArr[i].charAt(0).toUpperCase().concat(newArr[i].substring(1, newArr[i].length)));
+  }
+  let result = secArr.join(" ");
+  return result;
+}
+
+titleCase("I'm a little tea pot");
+
+// titleCase("I'm a little tea pot") should return I'm A Little Tea Pot.
+// titleCase("sHoRt AnD sToUt") should return Short And Stout.
+// titleCase("HERE IS MY HANDLE HERE IS MY SPOUT") should return Here Is My Handle Here Is My Spout.
+
+// Second Solution (refractored):
+function titleCase(str) {
+  return str.split(" ").map((a) => a.toLowerCase()).map((a) => a.charAt(0).toUpperCase().concat(a.substring(1, a.length))).join(" ");
+}
+
+titleCase("I'm a little tea pot");
+
+// -----------------------------------------------
+// Basic Algorithm Scripting: Slice and Splice
+// You are given two arrays and an index.
+
+// Use the array methods slice and splice to copy each element of the first array into the second array, in order.
+
+// Begin inserting elements at index n of the second array.
+
+// Return the resulting array. The input arrays should remain the same after the function runs.
+
+function frankenSplice(arr1, arr2, n) {
+  // It's alive. It's alive!
+  let array = arr2.slice();
+  array.splice(n, 0, arr1)
+  let result = [].concat.apply([], array);
+  return result;
+}
+
+frankenSplice([1, 2, 3], [4, 5, 6], 1);
+
+// -----------------------------------------------
+// Basic Algorithm Scripting: Falsy Bouncer
+// Remove all falsy values from an array.
+
+// Falsy values in JavaScript are false, null, 0, "", undefined, and NaN.
+
+// Hint: Try converting each value to a Boolean.
+
+function bouncer(arr) {
+  // Don't show a false ID to this bouncer.
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++ ) {
+    if (!!arr[i] === true) {
+      newArr.push(arr[i]);
+    } 
+  }
+  return newArr;
+}
+
+bouncer([7, "ate", "", false, 9]);
+
+
+// -----------------------------------------------
+
+// Basic Algorithm Scripting: Where do I Belong
+// Return the lowest index at which a value (second argument) should be inserted into an array (first argument) once it has been sorted. The returned value should be a number.
+
+// For example, getIndexToIns([1,2,3,4], 1.5) should return 1 because it is greater than 1 (index 0), but less than 2 (index 1).
+
+// Likewise, getIndexToIns([20,3,5], 19) should return 2 because once the array has been sorted it will look like [3,5,20] and 19 is less than 20 (index 2) and greater than 5 (index 1).
+
+// Remember to use Read-Search-Ask if you get stuck. Write your own code.
+
+function getIndexToIns(arr, num) {
+  // Find my place in this sorted array.
+  arr.sort((a, b) => a - b);
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] >= num) {
+        return i;
+    }
+  }
+  return arr.length;
+}
+
+getIndexToIns([40, 60], 50);
+
+// -----------------------------------------------
+
+// Basic Algorithm Scripting: Mutations
+// Return true if the string in the first element of the array contains all of the letters of the string in the second element of the array.
+
+// For example, ["hello", "Hello"], should return true because all of the letters in the second string are present in the first, ignoring case.
+
+// The arguments ["hello", "hey"] should return false because the string "hello" does not contain a "y".
+
+// Lastly, ["Alien", "line"], should return true because all of the letters in "line" are present in "Alien".
+
+function mutation(arr) {
+  let arr1 = arr[0].toLowerCase();
+  let arr2 = arr[1].toLowerCase();
+
+  for (let i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) === -1) {
+      return false;
+    }
+  }
+  return true;
+}
+
+mutation(["Mary", "Aarmy"])
+
+// -----------------------------------------------
+
+
